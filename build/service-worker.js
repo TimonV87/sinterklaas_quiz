125eb0c22df53dacf0849096d211109ad2d79675
Timon <timon9441@protonmail.com>
2021-01-13 19:07:41 +0100

    Test

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/timonv87.github.io/sinterklaas_quiz/precache-manifest.257c9a6e878d79e6453b83ed20b572e9.js"
+  "/sinterklaas_quiz/precache-manifest.0c82aaead435776a890408dbc12ff39b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timonv87.github.io/sinterklaas_quiz/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sinterklaas_quiz/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
