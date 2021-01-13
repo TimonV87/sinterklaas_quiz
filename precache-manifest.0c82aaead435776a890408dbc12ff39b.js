ba953295ec9325dc4bb530609093a76530bc3355
Timon <timon9441@protonmail.com>
2021-01-13 19:00:14 +0100

    Updates

---
+++ precache-manifest.0c82aaead435776a890408dbc12ff39b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d21a02f3d17b6a583736755573048a6",
+    "url": "/sinterklaas_quiz/index.html"
+  },
+  {
+    "revision": "c8e891305a7aecb4bb95",
+    "url": "/sinterklaas_quiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "afffa3b2c049986c8dc8",
+    "url": "/sinterklaas_quiz/static/js/2.06d0882d.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/sinterklaas_quiz/static/js/2.06d0882d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e891305a7aecb4bb95",
+    "url": "/sinterklaas_quiz/static/js/main.72d78260.chunk.js"
+  },
+  {
+    "revision": "af2cea5f23b9b2d3ea88",
+    "url": "/sinterklaas_quiz/static/js/runtime-main.b0090fc8.js"
+  }
+]);
